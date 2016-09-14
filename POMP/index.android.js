b70a0da001892ebf733291d34228339f31e995fb
zx@junquan.com.cn <zx@junquan.com.cn>
2016-09-14 13:31:14 +0800

    android,ios的首页（登录页）

--- POMP/index.android.js
+++ POMP/index.android.js
@@ -6,48 +6,78 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TextInput
 } from 'react-native';
 
 class POMP extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+          <Image source={require('./images/login/logo.png')} style={styles.logo}></Image>
+          <View style={styles.border}>
+            <Image source={require('./images/login/userIcon.png')} style={styles.username}></Image>
+            <TextInput placeholder='用户名' placeholderTextColor='#999999' style={styles.inputText}></TextInput>
+          </View>
+          <View style={styles.border}>
+            <Image source={require('./images/login/pwdIcon.png')} style={styles.password}></Image>
+            <TextInput placeholder='密码' defaultValue="" secureTextEntry={true} placeholderTextColor='#999999' style={styles.inputText}></TextInput>
+          </View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex:1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#3dabec',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  logo:{
+    flexDirection:'row',
+    width:124,
+    height:124
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  inputText: {
+    width:300,
+    height:50,
+    fontSize: 14,
+    color:"#333",
+    borderColor:'transparent',
+    backgroundColor:'transparent'
   },
+  border:{
+    flexDirection:'row',
+    alignItems:'flex-start',
+    paddingLeft:10,
+    paddingRight:10,
+    paddingTop:0,
+    marginTop:20,
+    backgroundColor: '#6bc5fb',
+    borderRadius:50,
+    borderTopLeftRadius:50,
+    borderTopRightRadius:50,
+    borderBottomLeftRadius:50,
+    borderBottomRightRadius:50
+  },
+  username:{
+    flexDirection:'row',
+    marginTop:14,
+    width:24,
+    height:23
+  },
+  password:{
+    flexDirection:'row',
+    marginTop:12,
+    width:24,
+    height:28
+  }
 });
 
 AppRegistry.registerComponent('POMP', () => POMP);
